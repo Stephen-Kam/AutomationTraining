f4e4b589fd93c0a62afa0e29f807a090b8230d81
Stephen-Kam <stephen.kam@ten10.com>
2016-07-05 16:48:39 +0100

    Improved link checking method

--- Java/MercuryTours/src/test/java/com/Java/MercuryTours/Homepage.java
+++ Java/MercuryTours/src/test/java/com/Java/MercuryTours/Homepage.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import junit.framework.Assert;
 
 
 public class Homepage {
 
 	WebDriver driver;
+	
+	Testing testing = new Testing();
 	
 	//Top Navigation Links
 	@FindBy(xpath="html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a")
@@ -30,26 +31,12 @@ public class Homepage {
 		//List<WebElement> aList = driver.findElements(By.tagName("a"));
 		for (int i=0; i < aList.size(); i++) {
 			aList.get(i).click();
-			System.out.println(driver.getTitle());
-			checkText(i);
+			Assert.assertTrue("Check Text", driver.findElement(By.tagName("body")).getText().contains(testing.checkText(i)));
+			testing.checkTitle(driver, i);
 			driver.navigate().back();
 			//aList = driver.findElements(By.tagName("a"));
 			aList = driver.findElements(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/*"));
 		}
 	}
-	
-	public void checkText(int i) {
-		switch (i) {
-		case 0:
-			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Sign-on: Mercury Tours"));
-			break;
-		case 1:
-			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Register: Mercury Tours"));
-			break;
-		case 2-3:
-			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Under Construction: Mercury Tours"));
-			break;
-		}
-	}
-	
+
 }
---
+++ Java/MercuryTours/src/test/java/com/Java/MercuryTours/Testing.java
@@ -0,0 +1,39 @@
+package com.Java.MercuryTours;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+
+import junit.framework.Assert;
+
+public class Testing {
+	
+	public Testing() {
+	}
+	
+	public String checkText(int i){
+		List<String> stringList = new ArrayList<String>();
+		stringList.add("Welcome back to Mercury Tours");
+		stringList.add("To create your account");
+		stringList.add("This section of our web site");
+		stringList.add("This section of our web site");
+		
+		return stringList.get(i);
+	}
+	
+	public WebDriver checkTitle(WebDriver driver, int i) {
+		switch (i) {
+		case 0:
+			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Sign-on: Mercury Tours"));
+			break;
+		case 1:
+			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Register: Mercury Tours"));
+			break;
+		case 2-3:
+			Assert.assertTrue("Check title is correct", driver.getTitle().contains("Under Construction: Mercury Tours"));
+			break;
+		}
+		return driver;
+	}
+}
